1f9838313f0a968a1e5b95e10047a4d9b282c18a
wh0osadin <adzhra162@gmail.com>
2025-12-04 20:15:41 +0700

    Initialize color pairs in ncurses
    Added color initialization for ncurses.

--- main.cpp
+++ main.cpp
@@ -12,8 +12,18 @@ int main() {
     initscr();
     cbreak();
     noecho();
-    curs_set(0); // Sembunyikan kursor
-    keypad(stdscr, TRUE); // Aktifkan special keys (seperti arrow keys)
+    curs_set(0);
+    keypad(stdscr, TRUE);
+
+     // Inisialisasi warna
+    if (has_colors()) {
+        start_color();
+        init_pair(1, COLOR_WHITE, COLOR_BLACK);
+        init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(3, COLOR_RED, COLOR_BLACK);
+        init_pair(4, COLOR_GREEN, COLOR_BLACK);
+        init_pair(5, COLOR_CYAN, COLOR_BLACK); // Untuk input nama
+    }
     
     
     // Cleanup ncurses
