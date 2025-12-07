5ca3a6f42047a19bebe504955653f0a3662637f1
wh0osadin <adzhra162@gmail.com>
2025-12-07 22:03:18 +0700

    Define HighScoreEntry and HighScoreManager
    Added HighScoreEntry struct and HighScoreManager class.

--- source/highscore.h
+++ source/highscore.h
@@ -3,3 +3,15 @@
 #include <fstream>
 #include <cstring>
 #include <algorithm>
+
+struct HighScoreEntry {
+    char name[20];
+    int score;
+};
+
+class HighScoreManager {
+private:
+    HighScoreEntry highScores[10]; // Array fixed size untuk 10 high score
+    int scoreCount;
+    std::string filename;
+    const int MAX_SCORES = 10;
