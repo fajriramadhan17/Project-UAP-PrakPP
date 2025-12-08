90994f7e59790f011d355e42ec81e8710ec3ec82
wh0osadin <adzhra162@gmail.com>
2025-12-08 19:43:07 +0700

    Update game.h

--- source/game.h
+++ source/game.h
@@ -2,3 +2,16 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+
+// Struktur untuk player
+struct GameObject {
+    int x, y;
+    char symbol;
+};
+
+// Struktur untuk obstacle 3x3
+struct Obstacle3x3 {
+    int x, y; // Posisi tengah obstacle
+    char symbol;
+    char shape[3][3]; // Matriks 3x3
+};
